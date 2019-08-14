b35c08c58f89aff2538682c85eb2ca9f29a0f906
Rodrigo Duarte Silva Luz <rodrigodsluz@gmail.com>
2019-08-14 14:44:27 -0300

    new

--- C/Elisa's Class/Dynamic Memory Allocation/Second/main.c
+++ C/Elisa's Class/Dynamic Memory Allocation/Second/main.c
@@ -30,28 +30,46 @@ struct ALUNO{
     float nota;	
 };
 
+//(a) Alocar memória dinamicamente para um vetor do tipo ALUNO.
 struct ALUNO* studentArray(int n);
 
+//(b) Preencher o vetor de alunos.
 void fillStudentArray(int n, struct ALUNO* V);
 
+//(c) Imprimir o vetor de alunos.
 void printStudentArray(int n, struct ALUNO* V);
+
+//(d) Preencher o registro de um aluno.
+void fillStudentRecord(int n, struct ALUNO* V);
+	
+//(e) Imprimir o registro de um aluno.	
+void printStudentRecord(int n, struct ALUNO* V);
 
 int main(){
 	int n;
 	printf("How many students do you want to register? ");
 	scanf("%d", &n);
 	
+	//(a) Alocar memória dinamicamente para um vetor do tipo ALUNO.
 	struct ALUNO *V;
 	V = studentArray(n);
 	
+	//(b) Preencher o vetor de alunos.
 	fillStudentArray(n, V);
 	
-	printf("The student record is:\n");
+	//(c) Imprimir o vetor de alunos.
 	printStudentArray(n, V);
+	
+	//(d) Preencher o registro de um aluno.
+	fillStudentRecord(n, V);
+	
+	//(e) Imprimir o registro de um aluno.
+	printStudentRecord(n, V);
 	
 	return 0;
 }
 
+//(a) Alocar memória dinamicamente para um vetor do tipo ALUNO.
 struct ALUNO* studentArray(int n){
 	struct ALUNO* V;
 	V = (struct ALUNO*)malloc(n*sizeof(struct ALUNO));
@@ -64,40 +82,65 @@ struct ALUNO* studentArray(int n){
 	return V;
 }
 
+//(b) Preencher o vetor de alunos.
 void fillStudentArray(int n, struct ALUNO* V){
-	
+	printf("What are the student names?\n");
 	for(int i=0; i<n; i++){
-		printf("What is your registration number? ");
-		scanf("%d", &(V+i)->matricula);	
-		printf("What's your name? ");
+		printf("Student [%d] - ", i+1);
 		scanf(" %[^\n]", &(V+i)->nome);
-		printf("When you were born?\n");
+	}
+		
+}
+
+//(c) Imprimir o vetor de alunos.
+void printStudentArray(int n, struct ALUNO* V){
+	for(int i=0; i<n; i++){
+		printf("The student [%d] is: %s\n", i+1, (V+i)->nome);	
+	}
+	
+	
+}
+
+//(d) Preencher o registro de um aluno.
+void fillStudentRecord(int n, struct ALUNO* V){
+	printf("Registering the students record!\n");
+	for(int i=0; i<n; i++){
+		printf("Student [%d]\n", i+1);
+		printf("Name: %s\n", (V+i)->nome);
+		
+		printf("Registration number: ");
+		scanf("%d", &(V+i)->matricula);	
+		
+		printf("When he or she was born?\n");
 		printf("Day: ");
 		scanf("%d", &(V+i)->dia_nasc);
 		printf("Month: ");
 		scanf("%d", &(V+i)->mes_nasc);
 		printf("Year: ");
 		scanf("%d", &(V+i)->ano_nasc);
-		printf("What grade did you get on the test? ");
+		
+		printf("What grade did he or she get on the test? ");
 		scanf("%f", &(V+i)->nota);
 		
 	}
-}
-
-void printStudentArray(int n, struct ALUNO* V){
 	
+}
+	
+//(e) Imprimir o registro de um aluno.	
+void printStudentRecord(int n, struct ALUNO* V){
+	printf("The approved students are: \n");
 	for(int i=0; i<n; i++){
-		printf("Registration number: %d \n", (V+i)->matricula);
-	
-		printf("Name: %s \n", (V+i)->nome);
+		if((V+i)->nota>=6){
+			printf("Name: %s \n", (V+i)->nome);
 		
-		printf("Date of birth: %d/%d/%d \n", (V+i)->dia_nasc, (V+i)->mes_nasc, (V+i)->ano_nasc);
+			printf("Registration number: %d \n", (V+i)->matricula);
 		
-		printf("Grade: %.2f \n", (V+i)->nota);
+			printf("Date of birth: %d/%d/%d \n", (V+i)->dia_nasc, (V+i)->mes_nasc, (V+i)->ano_nasc);
 		
+			printf("Grade: %.2f \n", (V+i)->nota);
+		}	
 	}
+	
 }
-
-
 
 	
