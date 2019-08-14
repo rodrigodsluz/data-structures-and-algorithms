b7231a1c244ddb2dbe5a3be494c4eafcf0ec0a30
Rodrigo Duarte Silva Luz <rodrigodsluz@gmail.com>
2019-08-14 17:44:59 -0300

    new

---
+++ C#/Player.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour{
+	public int life;
+	public float speed;
+	public bool isFlying;
+	public string name;
+	
+    // Start is called before the first frame update
+    void Start(){
+        life = 5;
+    }
+
+    // Update is called once per frame
+    void Update(){
+        
+    }
+}
