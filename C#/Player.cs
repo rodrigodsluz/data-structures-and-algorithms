8d08512e688173b385040fb0353e51d2037602c7
Rodrigo Duarte Silva Luz <rodrigodsluz@gmail.com>
2019-08-15 12:07:25 -0300

    new

--- C#/Player.cs
+++ C#/Player.cs
@@ -4,17 +4,37 @@ using UnityEngine;
 
 public class Player : MonoBehaviour{
 	public int life;
-	public float speed;
 	public bool isFlying;
 	public string name;
 	
+	public float velocity;
+	public float distance;
+	public float time;
+	
+	public float maximumVelocity;
+	public float minimumVelocity;
+	
     // Start is called before the first frame update
     void Start(){
-        life = 5;
+        //checksVelocity();
     }
 
     // Update is called once per frame
     void Update(){
-        
+		if(Input.GetKeyDown(KeyCode.Space)){
+			checksVelocity();
+		}
     }
+	
+	void checksVelocity(){
+		velocity = distance/time;
+		Debug.Log("Your velocity is: "+ velocity);
+		if(velocity < minimumVelocity){
+			Debug.Log("You are so slow!");
+		}else if(velocity > maximumVelocity){
+			Debug.Log("You are so fast!");
+		}else{
+			Debug.Log("Congratulations! You are in the correct velocity!");
+		}
+	}
 }
