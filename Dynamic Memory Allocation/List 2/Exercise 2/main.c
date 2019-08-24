dfdd6991ebc18941f78a96d7dd266c2d3b27b3d6
Rodrigo Duarte Silva Luz <rodrigodsluz@gmail.com>
2019-08-24 14:44:03 -0300

    new

--- Dynamic Memory Allocation/List 2/Exercise 2/main.c
+++ Dynamic Memory Allocation/List 2/Exercise 2/main.c
@@ -1,13 +1,13 @@
 /*
 2. Escreva funções para lidar com matrizes triangulares inferiores de dimensão n * n, onde todos
-os elementos abaixo da diagonal principal são iguais a zero e não devem ser alocados. No entanto, um
+os elementos abaixo da diagonal são iguais a zero e não devem ser alocados. No entanto, um
 acesso a um elemento abaixo da diagonal deve retornar o valor zero. Escreva as seguintes
 funções, usando a estratégia de vetor de ponteiros para armazenar a matriz.
 (a) Função para criar uma matriz, onde n representa a dimensão da matriz, inicialmente com
 os valores todos iguais a zero:
 float **cria(int n);
 (b) Função para atribuir o valor de um elemento da matriz, assumindo que i <= j:
-void atribui(int i, int j, float x, float **m);
+void atribui(int i, int j, float x, float **m, int n);
 (c) Função para acessar o valor de um elemento da matriz, inclusive elementos acima da
 diagonal:
 float acessa(int i, int j);
@@ -21,24 +21,29 @@ Escreva uma função main para testar as funções implementadas.
 
 float **cria(int n);
 
-void atribui(int i, int j, int x, float **m, int n);
+void atribui(int i, int j, float x, float **m, int n);
+
+float acessa(int i, int j, float **m, int n);
+
+void libera(int n, float **m);
 
 int main(){
 	int n;
 	printf("Enter the size: ");
 	scanf("%d", &n);
 	
+	//(a)
 	float **m = cria(n);
 	
+	//(b)
 	printf("Enter the values:\n");
-	int x;
+	float x;
 	for(int i=0; i<n; i++){
 		for(int j=0; j<n; j++){
 			scanf("%f ", &x);
 			atribui(i, j, x, m, n);
 		}
 	}
-	
 	
 	for(int i=0; i<n; i++){
 		for(int j=0; j<n; j++){
@@ -47,8 +52,21 @@ int main(){
 		printf("\n");
 	}
 	
+	//(c)
+	int a, b;
+	printf("Enter the row: ");
+	scanf("%d", &a);
+	printf("Enter the column: ");
+	scanf("%d", &b);
 	
-	free(*m);
+	float value =  acessa(a, b, m, n);
+	
+	printf("%.2f", value);
+	
+	//(d)
+	libera(n, m);
+	
+	
 	return 0;
 }
 
@@ -59,22 +77,22 @@ float **cria(int n){
 		m[i] = (float*)calloc((n-i), sizeof(float));
 	}
 	
-	/*
-	if(i<=j){
-		for(int i=0; i<n; i++){
-			for(int j=0; j<n; j++){
-				newMatrix[i][j] = 0;
-			}
-		}
-	}
-	*/
-	
 	return m;
 	
 }
 
-void atribui(int i, int j, int x, float **m, int n){
-	if(i>=j){
+void atribui(int i, int j, float x, float **m, int n){
+	if(i<=j){
 		m[i][j-i] = x;
 	}
+}
+
+float acessa(int i, int j, float **m, int n){
+	float value =  m[i][j];
+	
+	return value;
+}
+
+void libera(int n, float **m){
+	free(*m);
 }
---
+++ Recursion/Exercise 2/main.c
@@ -0,0 +1,20 @@
+/*
+4. A seguinte função calcula o maior divisor comum dos inteiros positivos m e n. Escreva uma
+função recursiva equivalente.
+1. int Euclides(int m, int n){
+2. int r;
+3. do{
+4. r = m % n;
+5. m = n;
+6. n = r;
+7. while(r != 0);
+8. return m;}
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	
+	return 0;
+}
