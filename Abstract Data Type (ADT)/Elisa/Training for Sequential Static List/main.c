f639a27b4a37d9a3353201f3ba78d3c4e7eff586
Rodrigo Duarte Silva Luz <rodrigodsluz@gmail.com>
2019-09-24 11:16:10 -0300

    new

---
+++ Abstract Data Type (ADT)/Elisa/Training for Sequential Static List/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "staticList.h"
+
+int main(){
+    
+
+
+    return 0;
+}
---
+++ Abstract Data Type (ADT)/Elisa/Training for Singly Linked List/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "linkedList.h"
+
+int main(){
+
+
+    return 0;
+}
