6cd70e8f94b8a88ff3e31b8f80d9ba2b7e66ec5e
Rodrigo Duarte Silva Luz <rodrigodsluz@gmail.com>
2019-10-05 11:08:15 -0300

    new

--- Abstract Data Type (ADT)/Data Structure/2 - Linked List/3 - List 5/mainListaEncadeada.c
+++ Abstract Data Type (ADT)/Data Structure/2 - Linked List/3 - List 5/mainListaEncadeada.c
@@ -1,8 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-//#include "listaCircularDuplaEnc.h"
-//#include "listaCircularEnc.h"
-//#include "listaDuplaEnc.h"
 #include "listaEncadeada.h"
 
 int addStudentRegistration(Student *student);
