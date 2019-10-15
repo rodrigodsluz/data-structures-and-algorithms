46f1556f3dd7e76062211454ccc17bb3dd466383
Rodrigo Duarte Silva Luz <rodrigodsluz@gmail.com>
2019-10-15 16:26:58 -0300

    new

---
+++ Abstract Data Type (ADT)/Data Structure/3 - Queue/4 - My Static Queue/adt.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "adt.h"
+#define MAX 100
+
+struct queue{
+    int head, tail, amount;
+    Student data[MAX];
+};
+
+Queue *createQueue(){
+    Queue *qu = (Queue*) malloc(sizeof(Queue));
+    if(qu != NULL){
+        qu->inicio = 0;
+        qu->final = 0;
+        qu->amount = 0;
+    }
+    return fi;
+}
+
+int freeQueue(Queue *qu){
+    free(qu); 
+}
+
+int insertInTheQueue(Queue *qu, Student stud){
+    if(qu == NULL)
+        return 0;
+
+    if(qu->amount = MAX)
+        return 0;
+
+    qu->data[qu->tail] = stud;
+    qu->tail = (qu->tail+1)%MAX;
+    qu->amount++;
+    
+    return 1;
+}
