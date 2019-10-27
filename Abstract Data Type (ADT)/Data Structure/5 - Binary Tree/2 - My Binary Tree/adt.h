747c835c2727036f1bf3776b0fee66eccd28a992
Rodrigo Duarte Silva Luz <rodrigodsluz@gmail.com>
2019-10-27 06:51:19 -0300

    new

---
+++ Abstract Data Type (ADT)/Data Structure/5 - Binary Tree/2 - My Binary Tree/adt.h
@@ -0,0 +1,32 @@
+// ADT for linked binary tree
+#ifndef _BINARY_TREE_
+#define _BINARY_TREE_
+
+// Definition of abstracts type
+typedef struct node Node;
+typedef Node* BinaryTree;
+
+// Functions to allocate e deallocate memory
+BinaryTree  *createBinaryTree();
+int freeBinaryTree(BinaryTree *root);
+
+// funções para obter informações da arvore binaria
+int quantidade_nos_arvore_binaria(ArvoreBin *raiz);
+int altura_arvore_binaria(ArvoreBin *raiz);
+int esta_vazia_arvore_binaria(ArvoreBin *raiz);
+
+// função para inserção de elementos da arvore binaria
+int inserir_arvore_binaria(ArvoreBin *raiz, int valor);
+
+// função para remoção de elementos da arvore binaria
+int remover_arvore_binaria(ArvoreBin *raiz, int valor);
+
+// função para buscar elementos na arvore binaria
+int consultar_arvore_binaria(ArvoreBin *raiz, int valor);
+
+// funções para percorrer a arvore binaria
+void percorrer_em_ordem_arvore_binaria(ArvoreBin *raiz);
+void percorrer_pre_ordem_arvore_binaria(ArvoreBin *raiz);
+void percorrer_pos_ordem_arvore_binaria(ArvoreBin *raiz);
+
+#endif
