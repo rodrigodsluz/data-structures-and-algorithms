10e2252a8970dd2a491cc9b90de0ba11d404abdb
RodrigoDuarte655 <rodrigodsluz@gmail.com>
2020-01-05 10:21:29 -0300

    new

--- Algorithms and Data Structures II/Sorting Algorithms/5 - heapSort/adt.c
+++ Algorithms and Data Structures II/Sorting Algorithms/5 - heapSort/adt.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include "adt.h"
 
-void Refaz(int left, int right, itemType *A)
+void remake(int left, int right, itemType *A)
 {
   int i = left;
   int j; itemType x;
@@ -10,38 +10,38 @@ void Refaz(int left, int right, itemType *A)
   x = A[i];
   while (j <= right){
     if (j < right)
-      if (A[j].Chave < A[j+1].Chave) j++;
-    if (x.Chave >= A[j].Chave) break;
+      if (A[j].key < A[j+1].key) j++;
+    if (x.key >= A[j].key) break;
     A[i] = A[j];
     i = j;
-    j = i*2;
+    j = i*2;  
   }
   A[i] = x;
 }
 
-void Constroi(itemType *A, int n)
+void build(itemType *A, int n)
 {
   int left;
   left = n / 2 + 1;
   while (left > 1){
     left--;
-    Refaz(left, n, A);
+    remake(left, n, A);
   }
 }
 
 void heapSort(itemType *A, int n)
 {
-    int esq, dir;
+    int left2, right2;
     itemType x;
 
-    Constroi(A, n);
-    esq = 1;
-    dir = n;
-    while(dir > 1){
+    build(A, n);
+    left2 = 1;
+    right2 = n;
+    while(right2 > 1){
         x = A[1];
-        A[1] = A[dir];
-        A[dir] = x;
-        dir--;
-        Refaz(esq, dir, A);
+        A[1] = A[right2];
+        A[right2] = x;
+        right2--;
+        remake(left2, right2, A);
     }
 }
--- Algorithms and Data Structures II/Sorting Algorithms/5 - heapSort/main.c
+++ Algorithms and Data Structures II/Sorting Algorithms/5 - heapSort/main.c
@@ -22,9 +22,9 @@ int main (int argc, char *argv[])
     itemType *A;
     int n = 10, i;
 
-    A = calloc(sizeof(itemType), n);
+    A = calloc(sizeof(itemType), 2*n); //+1 para a sentinela em P[0]
     printf("\nUnsorted Array: \n");
-    for (i=0; i<n; i++){
+    for (i=1; i<=n; i++){
         A[i].key = rand() % 100;
         printf (" %d",A[i].key);
     }
