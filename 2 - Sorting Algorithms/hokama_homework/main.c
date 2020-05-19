ad887d372daf09cc202dbaadb82e733ae2d814f4
rodrigodsluz <rodrigodsluz@gmail.com>
2020-05-19 20:27:48 -0300

    [FIX] Hokama homework updated

--- 2 - Sorting Algorithms/hokama_homework/main.c
+++ 2 - Sorting Algorithms/hokama_homework/main.c
@@ -21,8 +21,6 @@ int main()
     //Alocando espaço na memória para o struct champion
     champion *C = (champion *)calloc(n, sizeof(champion));
 
-    printf("\n---------INPUT---------\n\n");
-
     //Pegar dados do usuário (um nome sem espaços e um inteiro)
     for (int i = 0; i < n; i++)
     {
@@ -33,13 +31,14 @@ int main()
     //Chamada da função para ordenar os números em estratos e os nomes entre eles em ordem alfabética
     selectionSort(C, n);
 
-    printf("\n---------OUTPUT---------\n\n");
-
     //Imprimindo os valores de saída ordenados
     for (int i = 0; i < n; i++)
     {
         printf("%s %d\n", C[i].name, C[i].tier);
     }
+
+    //Libera espaço alocado na memória
+    free(C);
 
     return 0;
 }
@@ -47,7 +46,7 @@ int main()
 //Implementação da função de ordenação por seleção
 void selectionSort(champion *C, int n)
 {
-    int i, j, min = 0;
+    int i = 0, j = 0, menor = 0;
     champion x;
 
     //Alocando espaço na memória para o struct temporário
@@ -56,30 +55,33 @@ void selectionSort(champion *C, int n)
     for (i = 0; i < n - 1; i++)
     {
         //Procura o menor elemento em relação a "i"
-        min = i;
+        menor = i;
         for (j = i + 1; j < n; j++)
         {
-            if (C[j].tier < C[min].tier)
+            if (C[j].tier < C[menor].tier)
             {
-                min = j;
+                menor = j;
             }
 
             //Ordena os nomes que estão entre cada estrato em ordem alfabética
-            if (C[j].tier == C[min].tier)
+            if (C[j].tier == C[menor].tier)
             {
                 //Se o nome do personagem que está em determinado estrato for alfabeticamente maior que o próximo será retornado uma valor > 0
-                if (strcmp(C[min].name, C[j].name) > 0)
+                if (strcmp(C[menor].name, C[j].name) > 0)
                 {
-                    //Essa função copia o valor da variável do segundo parâmetro para a variável do primeiro. E com a ajuda da variável temporária é possível ordenar
+                    //Essa função copia o valor da variável do segundo parâmetro para a variável do primeiro. E com a ajuda da variável temporária é possível ordenar alfabeticamente
                     strcpy(temp->name, C[j].name);
-                    strcpy(C[j].name, C[min].name);
-                    strcpy(C[min].name, temp->name);
+                    strcpy(C[j].name, C[menor].name);
+                    strcpy(C[menor].name, temp->name);
                 }
             }
         }
         //Troca os valores da posição atual com a "menor"
-        x = C[min];
-        C[min] = C[i];
+        x = C[menor];
+        C[menor] = C[i];
         C[i] = x;
     }
+
+    //Libera espaço alocado na memória
+    free(temp);
 }
